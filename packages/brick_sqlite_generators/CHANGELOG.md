e7d0c99c2b3bce90c95dc9b341f8baf354488cbc
Tim Shedor <tshedor@greenbits.com>
2020-02-24 17:26:13 -0800

    type arguments are stripped in fieldsToSqliteColumns (#31)

--- packages/brick_sqlite_generators/CHANGELOG.md
+++ packages/brick_sqlite_generators/CHANGELOG.md
@@ -1,1 +1,3 @@
 ## Unreleased
+
+* Type arguments are stripped from fields when building the `fieldsToSqliteColumns` definition (#31)
---
+++ packages/brick_sqlite_generators/test/sqlite_model_serdes_generator/test_field_with_type_argument.dart
@@ -0,0 +1,66 @@
+import 'package:brick_sqlite_abstract/annotations.dart';
+import 'package:brick_sqlite_abstract/sqlite_model.dart';
+
+final output = r"""
+// GENERATED CODE DO NOT EDIT
+// This file should NOT be version controlled and should not be manually edited.
+part of '../brick.g.dart';
+
+Future<FieldWithTypeArgument> _$FieldWithTypeArgumentFromSqlite(
+    Map<String, dynamic> data,
+    {SqliteProvider provider,
+    SqliteFirstRepository repository}) async {
+  return FieldWithTypeArgument(
+      someField:
+          data['some_field'] == null ? null : jsonDecode(data['some_field']))
+    ..primaryKey = data['_brick_id'] as int;
+}
+
+Future<Map<String, dynamic>> _$FieldWithTypeArgumentToSqlite(
+    FieldWithTypeArgument instance,
+    {SqliteProvider provider,
+    SqliteFirstRepository repository}) async {
+  return {'some_field': jsonEncode(instance.someField ?? {})};
+}
+
+/// Construct a [FieldWithTypeArgument]
+class FieldWithTypeArgumentAdapter
+    extends SqliteAdapter<FieldWithTypeArgument> {
+  FieldWithTypeArgumentAdapter();
+
+  final Map<String, Map<String, dynamic>> fieldsToSqliteColumns = {
+    'primaryKey': {
+      'name': '_brick_id',
+      'type': int,
+      'iterable': false,
+      'association': false,
+    },
+    'someField': {
+      'name': 'some_field',
+      'type': Map,
+      'iterable': false,
+      'association': false,
+    }
+  };
+  Future<int> primaryKeyByUniqueColumns(
+          FieldWithTypeArgument instance, DatabaseExecutor executor) async =>
+      null;
+  final String tableName = 'FieldWithTypeArgument';
+
+  Future<FieldWithTypeArgument> fromSqlite(Map<String, dynamic> input,
+          {provider, repository}) async =>
+      await _$FieldWithTypeArgumentFromSqlite(input,
+          provider: provider, repository: repository);
+  Future<Map<String, dynamic>> toSqlite(FieldWithTypeArgument input,
+          {provider, repository}) async =>
+      await _$FieldWithTypeArgumentToSqlite(input,
+          provider: provider, repository: repository);
+}
+""";
+
+@SqliteSerializable()
+class FieldWithTypeArgument extends SqliteModel {
+  final Map<String, dynamic> someField;
+
+  FieldWithTypeArgument(this.someField);
+}
