dbb550b1eecb58bc781bcd66c53500b383ae5a0e
Tim Shedor <tshedor@gmail.com>
2021-01-10 09:51:11 -0800

    add more changelog documentation for this chunk

--- packages/brick_sqlite/lib/sqlite.dart
+++ packages/brick_sqlite/lib/sqlite.dart
@@ -2,3 +2,4 @@ export 'src/sqlite_adapter.dart';
 export 'src/runtime_sqlite_column_definition.dart';
 export 'src/sqlite_model_dictionary.dart';
 export 'src/sqlite_provider.dart';
+export 'package:brick_sqlite_abstract/sqlite_model.dart';
--- packages/brick_sqlite/lib/src/sqlite_model_dictionary.dart
+++ packages/brick_sqlite/lib/src/sqlite_model_dictionary.dart
@@ -1,7 +1,6 @@
 import 'package:brick_core/core.dart';
 import 'package:brick_sqlite/src/sqlite_adapter.dart';
 import 'package:brick_sqlite_abstract/sqlite_model.dart';
-export 'package:brick_sqlite_abstract/sqlite_model.dart';
 
 /// Associates app models with their [SqliteAdapter]
 class SqliteModelDictionary extends ModelDictionary<SqliteModel, SqliteAdapter<SqliteModel>> {
