70ec41ef9881901169aa33cbcb80d00ed4e3b00c
Tim Shedor <tshedor@gmail.com>
2021-01-18 14:48:52 -0800

    add exists method to provider (#120)

--- packages/brick_core/lib/src/provider.dart
+++ packages/brick_core/lib/src/provider.dart
@@ -13,6 +13,11 @@ abstract class Provider<_Model extends Model> {
   /// Remove a model instance
   delete<T extends _Model>(T instance, {Query query, ModelRepository<_Model> repository});
 
+  /// Whether a model instance is present. `null` is returned when existence is unknown.
+  /// The model instance is not hydrated in the function output; a `bool` variant
+  /// (e.g. `List<bool>`, `Map<_Model, bool>`) should be returned.
+  exists<T extends _Model>({Query query, ModelRepository<_Model> repository}) => null;
+
   /// Query for raw data and construct it with an [Adapter]
   get<T extends _Model>({Query query, ModelRepository<_Model> repository});
 
