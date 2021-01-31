c66109f5979f28b1a807a8d72ca2d6fbc9c0012a
Tim Shedor <tshedor@gmail.com>
2021-01-30 16:40:22 -0800

    add tests for new mixins

--- packages/brick_offline_first/lib/src/mixins/delete_all_mixin.dart
+++ packages/brick_offline_first/lib/src/mixins/delete_all_mixin.dart
@@ -1,5 +1,6 @@
 import 'package:brick_offline_first/offline_first.dart';
 import 'package:brick_offline_first/offline_first_with_rest.dart';
+import 'package:meta/meta.dart';
 
 /// Adds functions [deleteAll] and [deleteAllExcept]
 mixin DeleteAllMixin<T extends OfflineFirstModel> on OfflineFirstRepository<T> {
@@ -18,7 +19,11 @@ mixin DeleteAllMixin<T extends OfflineFirstModel> on OfflineFirstRepository<T> {
 
   /// The convenient inverse of [deleteAll]. [query] defines the instances that **should not**
   /// be deleted. Return value reflects if the operation completed without any failures.
-  Future<bool> deleteAllExcept<_Model extends T>({Query query}) async {
+  ///
+  /// It is **strongly recommended** to use Equatable or to override the `==` operator in
+  /// your app's models when incorporating this method. A delta between models to keep and
+  /// models to remove is computed with `==`.
+  Future<bool> deleteAllExcept<_Model extends T>({@required Query query}) async {
     final allModels = await get<_Model>();
     final modelsToKeep = await get<_Model>(query: query);
     final modelsToDelete = allModels.where((m) => !modelsToKeep.contains(m));
