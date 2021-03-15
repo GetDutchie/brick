4b3282db3ff4f6ab84fdca130240367baa1f72c6
Tim Shedor <tshedor@gmail.com>
2021-03-14 17:24:00 -0700

    cleanup

--- packages/brick_sqlite/lib/memory_cache_provider.dart
+++ packages/brick_sqlite/lib/memory_cache_provider.dart
@@ -64,8 +64,8 @@ class MemoryCacheProvider extends Provider<SqliteModel> {
     // If this query is searching for a unique identifier, return that specific record
     final byId = Where.firstByField(InsertTable.PRIMARY_KEY_FIELD, query?.where);
     if (byId?.value != null) {
-      final object = managedObjects[_Model]![byId!.value] as _Model;
-      if (object != null) return [object];
+      final object = managedObjects[_Model]?[byId!.value];
+      if (object != null) return [object as _Model];
     }
 
     return null;
