f038a7166e284d36ff186d19a28e8cfaa198ef2e
Tim Shedor <tshedor@greenbits.com>
2021-05-16 14:47:56 -0700

    honor locked requests in the offline queue (null safe) (#173)

--- packages/brick_offline_first/test/offline_queue/request_sqlite_cache_manager_test.dart
+++ packages/brick_offline_first/test/offline_queue/request_sqlite_cache_manager_test.dart
@@ -4,6 +4,7 @@ import 'package:brick_offline_first/src/offline_queue/request_sqlite_cache_manag
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sqflite_common/sqlite_api.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:http/http.dart' as http;
 import '__helpers__.dart';
 
 void main() {
@@ -49,23 +50,7 @@ void main() {
       final asCacheItem = RequestSqliteCache(request);
       await asCacheItem.insertOrUpdate(await _requestManager.getDb());
       final req = await _requestManager.prepareNextRequestToProcess();
-      expect(req!.method, 'PUT');
-    });
-
-    test('#prepareNextRequestToProcess', () async {
-      final inner = stubResult(statusCode: 501);
-      final client = OfflineQueueHttpClient(inner, requestManager);
-
-      await client.post(Uri.parse('http://0.0.0.0:3000'), body: 'existing record');
-      await client.put(Uri.parse('http://0.0.0.0:3000'), body: 'existing record');
-
-      final request = await requestManager.prepareNextRequestToProcess();
-      expect(request!.method, 'POST');
-
-      final asCacheItem = RequestSqliteCache(request);
-      await asCacheItem.insertOrUpdate(await requestManager.getDb());
-      final req = await requestManager.prepareNextRequestToProcess();
-      expect(req!.method, 'POST');
+      expect(req?.method, 'PUT');
     });
 
     test('#deleteUnprocessedRequest', () async {
@@ -80,6 +65,91 @@ void main() {
       await requestManager
           .deleteUnprocessedRequest(unprocessedRequests[0][HTTP_JOBS_PRIMARY_KEY_COLUMN]);
       expect(await requestManager.unprocessedRequests(), isEmpty);
+    });
+
+    group('#prepareNextRequestToProcess', () {
+      test('integration', () async {
+        final inner = stubResult(statusCode: 501);
+        final client = OfflineQueueHttpClient(inner, requestManager);
+
+        await client.post(Uri.parse('http://0.0.0.0:3000'), body: 'existing record');
+        await client.put(Uri.parse('http://0.0.0.0:3000'), body: 'existing record');
+
+        final request = await requestManager.prepareNextRequestToProcess();
+        expect(request?.method, 'POST');
+
+        final asCacheItem = RequestSqliteCache(request!);
+        await asCacheItem.insertOrUpdate(await requestManager.getDb());
+        // Do not retry request if the row is locked and serial processing is active
+        final req = await requestManager.prepareNextRequestToProcess();
+        expect(req, isNull);
+      });
+
+      test('new request is locked and skipped', () async {
+        final request = http.Request('POST', Uri.parse('http://localhost:3000/locked_request'));
+
+        // prepare unlocked request
+        final asCacheItem = RequestSqliteCache(request);
+        await asCacheItem.insertOrUpdate(await requestManager.getDb());
+
+        final requests = await requestManager.unprocessedRequests(onlyLocked: true);
+        expect(requests, hasLength(1));
+
+        final req = await requestManager.prepareNextRequestToProcess();
+        expect(req, isNull);
+      });
+
+      test('unlocked request is locked', () async {
+        final request = http.Request('POST', Uri.parse('http://localhost:3000/unlocked_request'));
+
+        // prepare unlocked request
+        final asCacheItem = RequestSqliteCache(request);
+        await asCacheItem.insertOrUpdate(await requestManager.getDb());
+        await asCacheItem.unlock(await requestManager.getDb());
+
+        final requests = await requestManager.unprocessedRequests(onlyLocked: false);
+        expect(requests, hasLength(1));
+
+        final lockedRequests = await requestManager.unprocessedRequests(onlyLocked: true);
+        expect(lockedRequests, isEmpty);
+
+        final req = await requestManager.prepareNextRequestToProcess();
+        expect(req?.url, Uri.parse('http://localhost:3000/unlocked_request'));
+
+        final newLockedRequests = await requestManager.unprocessedRequests(onlyLocked: true);
+        expect(newLockedRequests, hasLength(1));
+      });
+
+      test('locked request older than 2 minutes is unlocked', () async {
+        final request = http.Request('POST', Uri.parse('http://localhost:3000/old_request'));
+        final db = await requestManager.getDb();
+        // prepare unlocked request
+        final asCacheItem = RequestSqliteCache(request);
+        await asCacheItem.insertOrUpdate(await requestManager.getDb());
+        expect(await requestManager.prepareNextRequestToProcess(), isNull);
+
+        // ignore: invalid_use_of_protected_member
+        final response = await asCacheItem.findRequestInDatabase(db);
+        await db.update(
+          HTTP_JOBS_TABLE_NAME,
+          {
+            HTTP_JOBS_UPDATED_AT:
+                DateTime.now().subtract(Duration(seconds: 122)).millisecondsSinceEpoch,
+          },
+          where: '$HTTP_JOBS_PRIMARY_KEY_COLUMN = ?',
+          whereArgs: [response![HTTP_JOBS_PRIMARY_KEY_COLUMN]],
+        );
+
+        expect(await requestManager.unprocessedRequests(onlyLocked: true), hasLength(1));
+        expect(await requestManager.unprocessedRequests(onlyLocked: false), hasLength(1));
+
+        expect(await requestManager.prepareNextRequestToProcess(), isNull);
+
+        final updatedReq = await requestManager.prepareNextRequestToProcess();
+        expect(updatedReq?.url, Uri.parse('http://localhost:3000/old_request'));
+
+        expect(await requestManager.unprocessedRequests(onlyLocked: true), hasLength(1));
+      });
     });
   });
 }
