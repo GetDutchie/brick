20ea4f247e7af83d7f128919a56a4ccd1d42046f
Tim Shedor <tshedor@greenbits.com>
2021-08-02 10:21:51 -0700

    do not rerun timer before queue operation has completed (#195)

--- packages/brick_offline_first/lib/src/offline_queue/offline_request_queue.dart
+++ packages/brick_offline_first/lib/src/offline_queue/offline_request_queue.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:http/http.dart' as http;
 import 'package:logging/logging.dart';
 import 'package:meta/meta.dart';
 import 'offline_queue_http_client.dart';
@@ -13,6 +14,10 @@ class OfflineRequestQueue {
 
   final Logger _logger;
 
+  /// This mutex ensures that concurrent writes to the DB will
+  /// not occur as the Timer runs in sub routines or isolates
+  bool _processingInBackground = false;
+
   Timer _timer;
 
   OfflineRequestQueue({
@@ -24,20 +29,29 @@ class OfflineRequestQueue {
   void start() {
     stop();
     _logger.finer('Queue started');
+    _processingInBackground = false;
     _timer = Timer.periodic(client.requestManager.processingInterval, process);
   }
 
-  /// Invalidates timer. This does not stop actively-running recreated jobs.
+  /// Invalidates timer. This does not stop actively running work.
   void stop() {
     _timer?.cancel();
     _timer = null;
+    _processingInBackground = false;
     _logger.finer('Queue stopped');
   }
 
   /// Resend latest unproccessed request to the client.
   @protected
   void process(Timer _timer) async {
-    final request = await client.requestManager.prepareNextRequestToProcess();
+    if (_processingInBackground) return;
+    _processingInBackground = true;
+    http.Request request;
+    try {
+      request = await client.requestManager.prepareNextRequestToProcess();
+    } finally {
+      _processingInBackground = false;
+    }
 
     if (request != null) {
       _logger.finest('Processing request ${request.method} ${request.url}');
