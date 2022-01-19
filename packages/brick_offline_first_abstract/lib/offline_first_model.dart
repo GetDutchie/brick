e4b111f64e8f87ef24e9c21746fbc0296c9c0240
Dalvin Josias Sejour <dalvin.josias@gmail.com>
2022-01-19 12:52:55 -0800

    Update packages/brick_offline_first_abstract/lib/offline_first_model.dart
    Co-authored-by: Tim Shedor <tshedor@gmail.com>

--- packages/brick_offline_first_abstract/lib/offline_first_model.dart
+++ packages/brick_offline_first_abstract/lib/offline_first_model.dart
@@ -1,6 +1,5 @@
 import 'package:brick_rest/rest.dart' show RestModel;
 import 'package:brick_sqlite_abstract/sqlite_model.dart';
-import 'package:brick_graphql/src/graphql_model.dart';
 
 /// This model is constructed by data in SQLite. It hydrates from a REST endpoint.
 ///
