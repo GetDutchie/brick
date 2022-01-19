109860e2eb420a4828199ed8c80779dd17d5f19e
Tim Shedor <tim.shedor@dutchie.com>
2022-01-19 13:24:56 -0800

    update changelog and graphql exports

--- packages/brick_graphql/lib/graphql.dart
+++ packages/brick_graphql/lib/graphql.dart
@@ -1,1 +1,8 @@
-import 'package:brick_graphql/src/graphql_model.dart';
+export 'package:brick_graphql/src/graphql_adapter.dart';
+export 'package:brick_graphql/src/graphql_model.dart';
+export 'package:brick_graphql/src/graphql_model_dictionary.dart';
+export 'package:brick_graphql/src/graphql_provider.dart';
+
+// Annotations
+export 'package:brick_graphql/src/annotations/graphql_serializable.dart';
+export 'package:brick_graphql/src/annotations/graphql.dart';
