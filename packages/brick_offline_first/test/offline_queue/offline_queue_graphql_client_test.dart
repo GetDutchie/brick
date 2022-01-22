8febe43707f5f0e52302ef2398787522d9666b4b
Dalvin Sejour <dalvinsejour@Dalvins-MacBook-Pro.local>
2022-01-22 02:53:38 -0800

    add test cases

--- packages/brick_offline_first/test/offline_queue/offline_queue_graphql_client_test.dart
+++ packages/brick_offline_first/test/offline_queue/offline_queue_graphql_client_test.dart
@@ -1,14 +1,10 @@
-import 'package:brick_offline_first/src/offline_queue/offline_graphql_request_queue.dart';
 import 'package:brick_offline_first/src/offline_queue/offline_queue_graphql_client.dart';
 import 'package:brick_offline_first/src/offline_queue/request_graphql_sqlite_cache_manager.dart';
-import 'package:brick_offline_first/src/offline_queue/rest_request_sqlite_cache_manager.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:gql_exec/gql_exec.dart';
-import 'package:http/testing.dart';
 import 'package:mockito/mockito.dart';
 import 'package:sqflite_common/sqlite_api.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-import 'package:brick_offline_first/src/offline_queue/offline_queue_http_client.dart';
 import 'package:gql/language.dart';
 import '__helpers__.dart';
 
@@ -39,7 +35,7 @@ void main() {
       final mockLink = MockLink();
       final client = OfflineQueueGraphqlClient(mockLink, requestManager);
 
-      final resp = await client
+      await client
           .request(
             Request(
               operation: Operation(
@@ -63,4 +59,30 @@ void main() {
       ).called(1);
     });
   });
+
+  test('#send forwards to inner client', () async {}, skip: 'Needs to be implemented');
+
+  test('requests are not tracked', () async {}, skip: 'Needs to be implemented');
+
+  test('request is stored in SQLite', () async {}, skip: 'Needs to be implemented');
+
+  test('request deletes after a successful response', () async {}, skip: 'Needs to be implemented');
+
+  test('request increments after a unsuccessful response', () async {},
+      skip: 'Needs to be implemented');
+
+  test('request creates and does not delete after an unsuccessful response', () async {},
+      skip: 'Needs to be implemented');
+
+  test('request is not deleted after sending to a misconfigured client', () async {},
+      skip: 'Needs to be implemented');
+
+  test('request is not deleted after sending to an inaccessible endpoint', () async {},
+      skip: 'Needs to be implemented');
+
+  test('request is not deleted after receiving a status code that should be reattempted',
+      () async {},
+      skip: 'Needs to be implemented');
+
+  test('.areGraphwlErrorsEmpty', () async {}, skip: 'Needs to be implemented');
 }
