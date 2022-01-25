16ec0912b4381676d0f4d1961eb1ea6b99d5d617
Dalvin Josias Sejour <dalvin.sejour@dutchie.com>
2022-01-25 11:29:08 -0800

    fix

--- packages/brick_offline_first/lib/src/offline_queue/offline_request_queue.dart
+++ packages/brick_offline_first/lib/src/offline_queue/offline_request_queue.dart
@@ -38,11 +38,7 @@ abstract class OfflineRequestQueue {
 
   /// Invalidates timer. This does not stop actively-running recreated jobs.
   void stop() {
-    try {
-      _timer?.cancel();
-    } catch (e) {
-      _timer = null;
-    }
+    _timer?.cancel();
     _timer = null;
     processingInBackground = false;
     logger.finer('Queue stopped');
