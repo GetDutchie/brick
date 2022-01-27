4f8a10782512bf901935a1da3a0becc8188761da
Tim Shedor <tim.shedor@dutchie.com>
2022-01-27 14:03:29 -0800

    tidy up

--- packages/brick_offline_first/lib/src/offline_first_with_graphql/offline_first_with_graphql_adapter.dart
+++ packages/brick_offline_first/lib/src/offline_first_with_graphql/offline_first_with_graphql_adapter.dart
@@ -1,12 +1,14 @@
 import 'package:brick_offline_first/src/offline_first_adapter.dart';
 
-import 'package:brick_rest/rest.dart' show RestProvider, RestAdapter;
-import 'package:brick_offline_first_abstract/abstract.dart' show OfflineFirstWithRestModel;
+import 'package:brick_graphql/graphql.dart' show GraphqlProvider, GraphqlAdapter;
+import 'package:brick_offline_first_abstract/abstract.dart' show OfflineFirstWithGraphqlModel;
 
 import 'package:brick_sqlite/sqlite.dart';
 
-/// This adapter fetches first from [SqliteProvider] then hydrates with [RestProvider].
-abstract class OfflineFirstWithRestAdapter<_Model extends OfflineFirstWithRestModel>
-    extends OfflineFirstAdapter<_Model> with RestAdapter<_Model> {
-  OfflineFirstWithRestAdapter();
+export 'package:brick_graphql/src/runtime_graphql_definition.dart';
+
+/// This adapter fetches first from [SqliteProvider] then hydrates with [GraphqlProvider].
+abstract class OfflineFirstWithGraphqlAdapter<_Model extends OfflineFirstWithGraphqlModel>
+    extends OfflineFirstAdapter<_Model> with GraphqlAdapter<_Model> {
+  OfflineFirstWithGraphqlAdapter();
 }
