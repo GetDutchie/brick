6d85b6f3585a85ad8be84bdd549431268d490813
Tim Shedor <tim.shedor@dutchie.com>
2022-01-28 11:37:40 -0800

    remove unused models

--- example_graphql/lib/app/models/customer.dart
+++ example_graphql/lib/app/models/customer.dart
@@ -1,5 +1,4 @@
 import 'package:brick_offline_first/offline_first_with_graphql.dart';
-import 'package:brick_offline_first/offline_first_with_rest.dart';
 import 'package:pizza_shoppe/app/models/pizza.dart';
 
 @ConnectOfflineFirstWithGraphql(
