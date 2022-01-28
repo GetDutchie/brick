96447c73648a6ab5980c37cd77d2eee9a275087c
Tim Shedor <tim.shedor@dutchie.com>
2022-01-27 16:46:07 -0800

    don't need this

--- packages/brick_offline_first/lib/src/offline_first_with_graphql/offline_first_with_graphql_repository.dart
+++ packages/brick_offline_first/lib/src/offline_first_with_graphql/offline_first_with_graphql_repository.dart
@@ -4,8 +4,6 @@ import 'package:brick_sqlite/memory_cache_provider.dart';
 import 'package:brick_offline_first/offline_first.dart';
 import 'package:meta/meta.dart';
 import 'package:gql_exec/gql_exec.dart';
-// ignore: implementation_imports
-import 'package:gql_link/src/exceptions.dart';
 
 import 'package:brick_graphql/graphql.dart' show GraphqlProvider;
 import 'package:brick_sqlite_abstract/db.dart' show Migration;
