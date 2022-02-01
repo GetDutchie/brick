d858e623a74069c2e29002b69f3db28874ca181b
Tim Shedor <tim.shedor@dutchie.com>
2022-02-01 08:43:34 -0800

    separate graphql from offline first abstract domain

---
+++ packages/brick_offline_first_with_graphql_abstract/lib/offline_first_model.dart
@@ -0,0 +1,4 @@
+import 'package:brick_offline_first_abstract/abstract.dart';
+import 'package:brick_graphql/graphql.dart' show GraphqlModel;
+
+abstract class OfflineFirstWithGraphqlModel extends OfflineFirstModel with GraphqlModel {}
