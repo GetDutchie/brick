4654f2c893233374008a3907c5bc4fd54100b039
Tim Shedor <tim.shedor@dutchie.com>
2022-02-25 00:00:33 -0800

    forgot test

---
+++ packages/brick_graphql_generators/test/graphql_model_serdes_generator/test_from_json_to_json.dart
@@ -0,0 +1,36 @@
+import 'package:brick_graphql/graphql.dart';
+
+final output = r'''
+Future<ToFromJson> _$ToFromJsonFromGraphql(Map<String, dynamic> data,
+    {required GraphqlProvider provider,
+    GraphqlFirstRepository? repository}) async {
+  return ToFromJson(assoc: ToFromJsonAssoc.fromJson(data['assoc']));
+}
+
+Future<Map<String, dynamic>> _$ToFromJsonToGraphql(ToFromJson instance,
+    {required GraphqlProvider provider,
+    GraphqlFirstRepository? repository}) async {
+  return {'assoc': instance.assoc.toJson()};
+}
+''';
+
+class ToFromJsonAssoc {
+  final int? integer;
+
+  ToFromJsonAssoc({
+    this.integer,
+  });
+
+  String toJson() => integer.toString();
+
+  factory ToFromJsonAssoc.fromJson(String data) => ToFromJsonAssoc(integer: int.tryParse(data));
+}
+
+@GraphqlSerializable()
+class ToFromJson {
+  final ToFromJsonAssoc? assoc;
+
+  ToFromJson({
+    this.assoc,
+  });
+}
