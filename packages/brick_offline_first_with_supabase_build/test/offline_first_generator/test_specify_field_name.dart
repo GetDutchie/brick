533154dc453fcfa26b44eb9d55a32a8bf89081a4
Tim Shedor <tshedor@gmail.com>
2024-08-19 08:49:30 -0700

    try again

--- packages/brick_offline_first_with_supabase_build/test/offline_first_generator/test_specify_field_name.dart
+++ packages/brick_offline_first_with_supabase_build/test/offline_first_generator/test_specify_field_name.dart
@@ -1,6 +1,6 @@
 import 'package:brick_offline_first_with_supabase/brick_offline_first_with_supabase.dart';
 import 'package:brick_sqlite/brick_sqlite.dart';
-import 'package:brick_supabase_abstract/brick_supabase_abstract.dart';
+import 'package:brick_supabase/brick_supabase.dart';
 
 final output = r'''
 Future<SpecifyFieldName> _$SpecifyFieldNameFromSupabase(Map<String, dynamic> data,
